9bd0f102ea461d1fbc323ff5bfd2a71545171340
fhilippedev <fhilippedev@gmail.com>
2023-10-09 19:45:51 -0300

    :up: Update

--- flatpak.sh
+++ flatpak.sh
@@ -15,5 +15,26 @@ flatpak install flathub org.telegram.desktop -y
 # Instala o audacity  via flatpak
 flatpak install flathub org.audacityteam.Audacity -y
 
-# Instala o autenticador  via flatpak
-flatpak install flathub com.belmoussaoui.Authenticator
+# Instala o Exrensio Manager via flatpak
+flatpak install flathub com.mattjakeman.ExtensionManager -y
+
+# Instala o Soundux via flatpak
+flatpak install flathub io.github.Soundux -y
+
+# Instala o Monitor do sistema via flatpak
+flatpak install flathub io.missioncenter.MissionCenter -y
+
+# Instala o Master PDF via flatpak
+flatpak install flathub net.codeindustry.MasterPDFEditor -y
+
+# Instala o  Gimp via flatpak
+flatpak install flathub org.gimp.GIMP -y
+
+# Instala o PulseAudio Volume Control via flatpak
+flatpak install flathub org.pulseaudio.pavucontrol -y
+
+# Instala o Telegram via flatpak
+flatpak install flathub org.telegram.desktop
+
+# Instala o  via flatpak
+flatpak install flathub org.upscayl.Upscayl
